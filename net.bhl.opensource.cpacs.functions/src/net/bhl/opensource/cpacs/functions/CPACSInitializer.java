f6b0b383b602735dd2485848a649adbb52a81f90
Marc Engelmann <marcengelmann@users.noreply.github.com>
2021-02-15 15:55:16 +0100

    licence typo fixed

--- net.bhl.opensource.cpacs.functions/src/net/bhl/opensource/cpacs/functions/CPACSInitializer.java
+++ net.bhl.opensource.cpacs.functions/src/net/bhl/opensource/cpacs/functions/CPACSInitializer.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * <copyright> Copyright (c) 2021 Bauhaus Luftfahrt e.V.. All rights reserved. This program and the accompanying
- * materials are made available under the terms of the Apache License v1.0 which accompanies this distribution,
+ * materials are made available under the terms of the Apache License v2.0 which accompanies this distribution,
  * and is available at http://www.apache.org/licenses/LICENSE-2.0 </copyright>
  *******************************************************************************/
 package net.bhl.opensource.cpacs.functions;
